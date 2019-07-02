048a8741ca6777f0c86964cbd582e2ea91fe2824
liujian <449667481@qq.com>
2019-07-03 02:43:29 +0800

    update

---
+++ public/m/js/loading.js
@@ -0,0 +1,160 @@
+
+var picArry = [
+	"/images/role_1_rc.png",
+	"/images/role_2_rc.png",
+	"/images/role_3_rc.png",
+	
+	
+	/*//人物序列帧
+	//1 hf
+	"/images/role/hf/1/000.png",
+	"/images/role/hf/1/001.png",
+	"/images/role/hf/1/002.png",
+	"/images/role/hf/1/003.png",
+	"/images/role/hf/1/004.png",
+	"/images/role/hf/1/005.png",
+	"/images/role/hf/1/006.png",
+	"/images/role/hf/1/007.png",
+	"/images/role/hf/1/008.png",
+	"/images/role/hf/1/009.png",
+	"/images/role/hf/1/010.png",
+	"/images/role/hf/1/011.png",
+	"/images/role/hf/1/012.png",
+	"/images/role/hf/1/013.png",
+	"/images/role/hf/1/014.png",
+	"/images/role/hf/1/015.png",
+	//1 rc
+	"/images/role/rc/1/000.png",
+	"/images/role/rc/1/001.png",
+	"/images/role/rc/1/002.png",
+	"/images/role/rc/1/003.png",
+	"/images/role/rc/1/004.png",
+	"/images/role/rc/1/005.png",
+	"/images/role/rc/1/006.png",
+	"/images/role/rc/1/007.png",
+	"/images/role/rc/1/008.png",
+	"/images/role/rc/1/009.png",
+	"/images/role/rc/1/010.png",
+	"/images/role/rc/1/011.png",
+	"/images/role/rc/1/012.png",
+	"/images/role/rc/1/013.png",
+	"/images/role/rc/1/014.png",
+	"/images/role/rc/1/015.png",
+	//2 hf
+	"/images/role/hf/2/000.png",
+	"/images/role/hf/2/001.png",
+	"/images/role/hf/2/002.png",
+	"/images/role/hf/2/003.png",
+	"/images/role/hf/2/004.png",
+	"/images/role/hf/2/005.png",
+	"/images/role/hf/2/006.png",
+	"/images/role/hf/2/007.png",
+	"/images/role/hf/2/008.png",
+	"/images/role/hf/2/009.png",
+	"/images/role/hf/2/010.png",
+	"/images/role/hf/2/011.png",
+	"/images/role/hf/2/012.png",
+	"/images/role/hf/2/013.png",
+	"/images/role/hf/2/014.png",
+	"/images/role/hf/2/015.png",
+	//2 rc
+	"/images/role/rc/2/000.png",
+	"/images/role/rc/2/001.png",
+	"/images/role/rc/2/002.png",
+	"/images/role/rc/2/003.png",
+	"/images/role/rc/2/004.png",
+	"/images/role/rc/2/005.png",
+	"/images/role/rc/2/006.png",
+	"/images/role/rc/2/007.png",
+	"/images/role/rc/2/008.png",
+	"/images/role/rc/2/009.png",
+	"/images/role/rc/2/010.png",
+	"/images/role/rc/2/011.png",
+	"/images/role/rc/2/012.png",
+	"/images/role/rc/2/013.png",
+	"/images/role/rc/2/014.png",
+	"/images/role/rc/2/015.png",
+	//3 hf
+	"/images/role/hf/3/000.png",
+	"/images/role/hf/3/001.png",
+	"/images/role/hf/3/002.png",
+	"/images/role/hf/3/003.png",
+	"/images/role/hf/3/004.png",
+	"/images/role/hf/3/005.png",
+	"/images/role/hf/3/006.png",
+	"/images/role/hf/3/007.png",
+	"/images/role/hf/3/008.png",
+	"/images/role/hf/3/009.png",
+	"/images/role/hf/3/010.png",
+	"/images/role/hf/3/011.png",
+	"/images/role/hf/3/012.png",
+	"/images/role/hf/3/013.png",
+	"/images/role/hf/3/014.png",
+	"/images/role/hf/3/015.png",
+	//3 rc
+	"/images/role/rc/3/000.png",
+	"/images/role/rc/3/001.png",
+	"/images/role/rc/3/002.png",
+	"/images/role/rc/3/003.png",
+	"/images/role/rc/3/004.png",
+	"/images/role/rc/3/005.png",
+	"/images/role/rc/3/006.png",
+	"/images/role/rc/3/007.png",
+	"/images/role/rc/3/008.png",
+	"/images/role/rc/3/009.png",
+	"/images/role/rc/3/010.png",
+	"/images/role/rc/3/011.png",
+	"/images/role/rc/3/012.png",
+	"/images/role/rc/3/013.png",
+	"/images/role/rc/3/014.png",
+	"/images/role/rc/3/015.png"*/
+];
+
+$(function(){
+
+	var count = 0;
+	var viewW = $(window).width();
+	viewW = viewW < 750 ? 750 : viewW;
+	var offsetW = (viewW - 750)/2;
+	//init
+	$('.loading-line.left').css("margin-left", viewW + 36);
+	$('.loading-line.right').css("margin-left", -(viewW + 36));
+	
+	
+	for (var i = 0; i < picArry.length; i++) {
+		//$('.main').css('background', 'url('+picArry[i]+') no-repeat -10000px -10000px');
+		var image = new Image();
+		image.src = picArry[i];
+		image.onload = function(){
+			count++;
+			var rate = count/picArry.length;
+			//动效
+			loadingRander(viewW, offsetW, rate);
+			//跳转
+			if(count >= picArry.length){
+				setTimeout(() => {
+					$('.loading-bg').hide();
+					$('.content').show();
+					//$('.main').css('background','');
+				}, 1000);
+			}
+		}
+		
+	}
+	//test
+	/*var rate = 0;
+	setInterval(() => {
+		rate += 0.1;
+		loadingRander(viewW, offsetW, rate);
+	}, 200);*/
+});
+
+function loadingRander(viewW, offsetW, rate){
+	if(rate >= 1){
+		rate = 1;
+	}
+	var num = Number(rate * 100).toFixed(0);
+	$('.loading-num').html(num);
+	$('.loading-line.left').css("margin-left", viewW + 36 - (viewW/2 * rate) - offsetW);
+	$('.loading-line.right').css("margin-left", -(viewW + 36 - (viewW/2 * rate) - offsetW));
+}
---
+++ public/m/js/move.js
@@ -0,0 +1,80 @@
+
+var topArry = [0, 655, 2400, 3256, 4500, 5700, 6974];
+var disabledScrollChangeNav = false;
+$(function(){
+
+	$('.part').hide();
+	$('.part-1').show();
+	$('.part-2').show();
+	$('.part .mide').addClass('test');
+	$('.footer').hide();
+	
+	//滚动条事件
+	$(document).scroll(function() {
+        var scroH = $(document).scrollTop();  //滚动高度
+        var viewH = $(window).height();  //可见高度 
+        var contentH = $(document).height();  //内容高度
+
+        console.log(scroH +"======"+viewH+"======="+contentH);
+
+        //part-2 
+        var timeLineMoveH = topArry[0];
+        if(scroH > timeLineMoveH && scroH < viewH + timeLineMoveH){  //距离顶部大于100px时
+        	$('.part-2').show();
+        }
+        //part-3
+        var roleMoveH = topArry[1];
+        if(scroH > roleMoveH && scroH < viewH + roleMoveH){
+        	$('.part-3').show();
+        }
+        // part-4
+        var part4 =  topArry[2];
+        if(scroH > part4 && scroH < viewH + part4){
+        	$('.part-4').show();
+        }
+        // part-5
+        var part5 =  topArry[3];
+        if(scroH > part5 && scroH < viewH + part5){
+        	$('.part-5').show();
+        }
+        // part-6
+        var part6 =  topArry[4];
+        if(scroH > part6 && scroH < viewH + part6){
+        	$('.part-6').show();
+        }
+        // part-7
+        var part7 =  topArry[5];
+        if(scroH > part7 && scroH < viewH + part7){
+        	$('.part-7').show();
+        }
+        // part-8
+        var part8 =  topArry[6];
+        if(scroH > part8 && scroH < viewH + part8){
+        	$('.part-8').show();
+        	$('.footer').show();
+        }
+        
+        
+ 
+    });
+	
+
+	//hf rc
+	$('.role_tag_item').click(function(){
+		var index = $(this).parent().index();
+		var roleBg = $('.role_bg').eq(index);
+		var isHf = $(roleBg).hasClass('hf');
+		if(isHf){
+			$('.role_bg').removeClass('hf');
+			$('.role_bg').addClass('rc');
+			$('.role_tag_item').removeClass('hf');
+			$('.role_tag_item').addClass('rc');
+		}else{
+			$('.role_bg').removeClass('rc');
+			$('.role_bg').addClass('hf');
+			$('.role_tag_item').removeClass('rc');
+			$('.role_tag_item').addClass('hf');
+		}
+	});
+	
+});
